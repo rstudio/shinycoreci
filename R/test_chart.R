76587bb68463c2ba4a9389341afbba86ba26686a
Barret Schloerke <schloerke@gmail.com>
2020-01-15 13:49:04 -0500

    test all jster apps on all platforms

--- R/test_chart.R
+++ R/test_chart.R
@@ -38,11 +38,5 @@ apps_shinyjster <- function() {
   df <- get_test_chart()
   apps <- df$App[df$shinyjster.done != ""]
 
-  if (is_mac()) {
-    apps <- setdiff(apps, "174-throttle-debounce")
-  } else if (is_windows()) {
-    apps <- setdiff(apps, "022-unicode-chinese")
-  }
-
   apps
 }
