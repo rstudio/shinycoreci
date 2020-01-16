58eb6cfca9f54fbe57498115e6c496a7ced789c2
Barret Schloerke <schloerke@gmail.com>
2020-01-16 13:47:51 -0500

    add view_test_diff

---
+++ R/view_test_diff.R
@@ -0,0 +1,40 @@
+find_bad_shinytest_files <- function(path = ".") {
+
+  folders <- strsplit(
+    dir(path, , pattern = "-current", recursive = TRUE, include.dirs = TRUE),
+    .Platform$file.sep
+  )
+  folder_names <- unique(vapply(folders, `[[`, character(1), 1))
+  folder_names
+}
+
+bad_shinytest_platform <- function() {
+  branch <- system("git rev-parse --abbrev-ref HEAD")
+
+  system_val <- tail(strsplit(branch, "-")[[1]], 1)
+  switch(
+    system_val,
+    "macOS" = "mac",
+    "Windows" = "win",
+    stop("unknown type: ", system_val)
+  )
+}
+
+
+#' View Shinytest Diff
+#'
+#' @param suffix Test output suffix to compare against
+#' @param path Root folder path
+#' @param ... Extra arguments passed to `shinytest::viewTestDiff`
+#' @export
+view_test_diff <- function(suffix = c("win", "mac"), path = ".", ...) {
+  if (missing(suffix)) {
+    suffix <- bad_shinytest_platform()
+  }
+  suffix <- match.arg(suffix)
+
+  folders <- find_bad_shinytest_files(path)
+  for (folder in folders) {
+    shinytest::viewTestDiff(folder, suffix = suffix, ...)
+  }
+}
