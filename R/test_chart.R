8c8f238f3124a906ea44d7208f4ea6de8185920e
Barret Schloerke <schloerke@gmail.com>
2020-02-20 12:04:16 -0500

    Consolidate test_chart and it's cache

--- R/test_chart.R
+++ R/test_chart.R
@@ -1,3 +1,14 @@
+# This cache is used for test chart data and package information.
+cache <- new.env()
+
+#' Clear cache for test chart and package info
+#'
+#' @export
+clear_cache <- function() {
+  names <- ls(cache, all.names = TRUE)
+  rm(list = names, envir = cache)
+}
+
 # Fetch spreadsheet with app testing information. Result is memoized. Use
 # `reset=TRUE` to clear cache.
 get_test_chart <- function() {
