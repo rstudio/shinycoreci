327a0e7ac97a8270e55ed6ad44d551b724239c95
Barret Schloerke <barret@rstudio.com>
2020-03-05 17:16:08 -0500

    Add run docker functions and RSPM api calls (#15)
    * Pull in docker and rspm functions from other repo
    
    * comment some docker code for now and update docs for rspm_install scripts functions
    
    * add some debug print statements
    
    * display the url
    
    * Better error message
    
    * greatly simplify the docker commands as they are to be build only by shinycoreci-apps github actions
    
    * rename param from docker_release to release
    
    * Document (GitHub Actions)
    
    * only allow certain R versions
    
    * remove magrittr dep
    
    * remove magrittr from DESCRIPTION file
    
    * namespace browseURL
    
    * Document (GitHub Actions)

---
+++ R/docker.R
@@ -0,0 +1,100 @@
+
+
+#' Clean up docker files
+#'
+#' @param stopped_containers boolean that determines if all stopped containers should be removed
+#' @param untagged_images boolean that determines of all untagged images should be removed
+#' @export
+docker_clean <- function(stopped_containers = TRUE, untagged_images = TRUE) {
+  if (isTRUE(stopped_containers)) {
+    message("Removing stopped containers")
+    try(docker_cmd("docker container prune --force"))
+  }
+  if (isTRUE(untagged_images)) {
+    message("Removing untagged images")
+    try(docker_cmd("docker rmi $(docker images --filter 'dangling=true' -q --no-trunc)"))
+  }
+  invisible()
+}
+
+
+
+#' Docker Testing
+#'
+#' @param r_version R version to use. Ex: \code{"3.6"}
+#' @param release Distro release name, such as "bionic" for ubuntu or "7" for centos
+#' @param port port to have server function locally
+#' @param launch_browser Logical variable that determines if the browser should open to the specified port location
+#' @describeIn docker Run SSO in a docker container
+#' @export
+docker_run_sso <- function(
+  release = c("bionic", "xenial", "centos7"),
+  port = switch(release, "centos7" = 7878, 3838),
+  r_version = "3.6",
+  launch_browser = TRUE
+) {
+  release <- match.arg(release)
+  r_version <- match.arg(r_version)
+
+  docker_run_server(
+    type = "sso",
+    release = release,
+    port = port,
+    r_version = r_version,
+    launch_browser = launch_browser
+  )
+}
+
+
+
+#' @describeIn docker Run SSP in a docker container
+#' @export
+docker_run_ssp <- function(
+  release = c("bionic", "xenial", "centos7"),
+  port = switch(release, "centos7" = 7878, 3838),
+  r_version = "3.6",
+  launch_browser = TRUE
+) {
+  release <- match.arg(release)
+  r_version <- match.arg(r_version)
+
+  docker_run_server(
+    type = "ssp",
+    release = release,
+    port = port,
+    r_version = r_version,
+    launch_browser = launch_browser
+  )
+}
+
+
+
+
+docker_run_server <- function(
+  type = c("sso", "ssp"),
+  release = c("bionic", "xenial", "centos7"),
+  port = switch(type,
+                sso = switch(release, "centos7" = 7878, 3838),
+                ssp = switch(release, "centos7" = 8989, 4949)
+                ),
+  r_version = "3.6",
+  launch_browser = launch_browser
+) {
+  if (isTRUE(launch_browser)) {
+    utils::browseURL(paste0("http://localhost:", port, "/"))
+  }
+  docker_cmd(
+    "docker run --rm -p ", port, ":3838 --name ", "sso_", r_version, "_", release, " rstudio/shinycoreci:", type, "-", r_version, "-", release
+  )
+}
+
+docker_cmd <- function(...) {
+  cmd <- paste0(...)
+  print(cmd)
+  ret <- system(cmd)
+  if (ret != 0 && ret != 2) {
+    # 0 is success
+    # 2 is interrupt
+    stop("docker command failed")
+  }
+}
---
+++ R/rspm.R
@@ -0,0 +1,164 @@
+
+# Info
+#   https://demo.rstudiopm.com/client/#/repos/3/overview
+# Has ID
+#   https://demo.rstudiopm.com/__api__/repos/4/packages?name=highcharter
+# Has sys reqs
+#   https://demo.rstudiopm.com/__api__/repos/4/packages/highcharter/sysreqs?id=593567&distribution=ubuntu&release=18.04
+
+# used in docker files
+
+#' RStudio Package Manager System Install Scripts
+#'
+#' @inheritParams apps_shinytest
+#' @param release Docker release to use. Such as \verb{'bionic'} or \verb{'centos7'}
+#' @describeIn rspm_install Install script needed for R packages to run
+#' @export
+rspm_install_scripts <- function(
+  dir = "apps",
+  release = c("bionic", "xenial", "centos7")
+) {
+  ret <- rspm_sys_reqs(
+    dir = dir,
+    release = match.arg(release)
+  )
+  ret$install_scripts
+}
+#' @describeIn rspm_install Install script needed for R packages to install
+#' @export
+rspm_pre_install_scripts <- function(
+  dir = "apps",
+  release = c("bionic", "xenial", "centos7")
+) {
+  ret <- rspm_sys_reqs(
+    dir = dir,
+    release = match.arg(release)
+  )
+  ret$pre_install_scripts
+}
+#' @describeIn rspm_install All install script needed for R packages
+#' @export
+rspm_all_install_scripts <- function(
+  dir = "apps",
+  release = c("bionic", "xenial", "centos7")
+) {
+  ret <- rspm_sys_reqs(
+    dir = dir,
+    release = match.arg(release)
+  )
+  ret$all_install_scripts
+}
+
+rspm_sys_reqs <- function(
+  dir = "apps",
+  release = c("bionic", "xenial", "centos7")
+) {
+
+  release <- match.arg(release)
+  distro_val <- rspm_distro(release)
+  release_val <- rspm_release(release)
+
+  message("Retrieving dependencies...")
+  deps <- app_deps(dir)$package
+
+  message("Query RSPM...")
+  pr <- progress::progress_bar$new(
+    total = length(deps),
+    format = paste0("[:current/:total, :eta/:elapsed] RSPM ", distro_val, "-", release_val, " deps: :name"),
+    show_after = 0,
+    clear = FALSE
+  )
+  reqs <- lapply(deps, function(dep) {
+    pr$tick(tokens = list(name = dep))
+    tryCatch({
+      rspm_pkg_reqs(dep, distro_val, release_val)
+    }, error = function(e) {
+      message("Error for dep '", dep, "'. Error: ", e)
+      NULL
+    })
+  })
+
+  list(
+    pre_install_scripts = rspm_output_txt(reqs, "pre_install", distro_val = distro_val),
+    install_scripts = rspm_output_txt(reqs, "install_scripts", distro_val = distro_val),
+    all_install_scripts = rspm_output_txt(reqs, c("pre_install", "install_scripts"), distro_val = distro_val)
+  )
+}
+
+rspm_pkg_reqs <- function(pkg_name, distro_val, release_val) {
+
+  # id <-
+  #   paste0("https://demo.rstudiopm.com/__api__/repos/4/packages?name=", pkg_name) %>%
+  #   jsonlite::fromJSON(simplifyDataFrame = FALSE) %>%
+  #   magrittr::extract2(1) %>%
+  #   magrittr::extract2("id")
+
+  info <- jsonlite::fromJSON(
+    paste0(
+      "https://demo.rstudiopm.com/__api__/repos/4/packages/", pkg_name, "/sysreqs",
+      "?distribution=", distro_val,
+      "&release=", release_val
+    ),
+    simplifyDataFrame = FALSE
+  )
+
+  list(
+    pre_install = rspm_output(info, "pre_install"),
+    install_scripts = rspm_output(info, "install_scripts")
+  )
+}
+
+
+rspm_release <- function(distro) {
+  switch(distro,
+    "xenial" = "14.04",
+    "bionic" = "18.04",
+    "centos6" = "6",
+    "centos7" = "7",
+    "centos8" = "8",
+    stop("Unknown distro: ", distro)
+  )
+}
+rspm_distro <- function(distro) {
+  switch(distro,
+    "xenial" = ,
+    "bionic" = "ubuntu",
+    "centos6" = ,
+    "centos7" = ,
+    "centos8" = "centos",
+    stop("Unknown distro: ", distro)
+  )
+}
+
+
+rspm_output <- function(info, key) {
+  unique(c(
+    info[[key]],
+    unlist(lapply(info$dependencies, `[[`, key))
+  ))
+}
+rspm_output_txt <- function(reqs, keys, distro_val) {
+  txt <- sort(unique(unlist(
+    lapply(keys, function(key) {
+      lapply(reqs, `[[`, key)
+    })
+  )))
+
+  if (length(txt) == 0) {
+    return("")
+  }
+
+  install_txt <- switch(
+    distro_val,
+    "centos" = "yum install -y ",
+    "ubuntu" = "apt-get install -y ",
+    stop("unknown distro: ", distro_val)
+  )
+  txt <- paste0(sub(install_txt, "", txt), collapse = " ")
+
+  paste0(
+    # "RUN ",
+    install_txt,
+    txt
+  )
+}
