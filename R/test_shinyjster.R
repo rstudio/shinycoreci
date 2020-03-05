4d137eb54b9e728e4d9723cb108cf2b6777c9cb4
Barret Schloerke <schloerke@gmail.com>
2020-03-05 16:21:13 -0500

    work with the lastest jster by passing in the browser

--- R/test_shinyjster.R
+++ R/test_shinyjster.R
@@ -2,18 +2,28 @@
 #'
 #' @param dir base folder to look for applications
 #' @param apps applications within \verb{dir} to run
-#' @inheritParams shinyjster::run_headless
+#' @param assert Logical value which will determine if \code{shinyjster::assert_jster} will be run on the final output. Defaults to \code{TRUE}
+#' @inheritParams shinyjster::run_jster_apps
 #' @export
 test_shinyjster <- function(
   dir = "apps",
   apps = apps_shinyjster(dir),
   port = 8000,
   host = "127.0.0.1",
-  debug_port = NULL,
-  browser = c("chrome", "firefox"),
+  browser = "chrome",
   type = c("serial", "lapply", "parallel", "callr"),
   assert = TRUE
 ) {
+
+  if (is.character(browser) && length(browser) == 1) {
+    browser <- switch(browser,
+      "chrome" = shinyjster::selenium_chrome(headless = TRUE),
+      "firefox" = shinyjster::selenium_chrome(headless = TRUE),
+      "edge" = shinyjster::selenium_edge(),
+      "ie" = shinyjster::selenium_ie(),
+      browser
+    )
+  }
 
   ras <- regular_and_source_apps(file.path(dir, apps), "_shinyjster.R")
 
@@ -22,14 +32,12 @@ test_shinyjster <- function(
 
   if (length(ras$regular) > 0) {
     regular_dt <-
-      shinyjster::run_headless(
+      shinyjster::run_jster_apps(
         apps = ras$regular,
         port = port,
         host = host,
-        debug_port = debug_port,
-        browser = match.arg(browser),
-        type = match.arg(type),
-        assert = FALSE
+        browser = browser,
+        type = match.arg(type)
       )
     regular_dt <- cbind(regular_dt, kind = "shiny", stringsAsFactors = FALSE)
   }
@@ -39,11 +47,9 @@ test_shinyjster <- function(
       test_shinyjster_source(
         apps = ras$source,
         args = list(
-          assert = FALSE,
-          type = "serial",
           port = port,
           host = host,
-          debug_port = debug_port
+          browser = browser
         )
       )
     source_dt <- cbind(source_dt, kind = "source", stringsAsFactors = FALSE)
