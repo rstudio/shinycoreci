d167cb18686dcf18a29442ea17e133791b36f146
Barret Schloerke <schloerke@gmail.com>
2020-07-27 14:41:55 -0400

    fix r cmd check failures

--- R/sysinfo.R
+++ R/sysinfo.R
@@ -48,7 +48,7 @@ write_sysinfo <- function(file = stdout()) {
 
   cat(
     utils::capture.output({
-      cat(osVersion, "\n", sep = "")
+      cat(utils::osVersion, "\n", sep = "")
       cat(rep("-", 80), "\n", sep = "")
       print(sessioninfo::platform_info())
       cat(rep("-", 80), "\n", sep = "")
