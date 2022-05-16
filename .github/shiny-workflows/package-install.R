e817ba50d357946c31252e3a1635c19128c404a2
Barret Schloerke <schloerke@gmail.com>
2022-05-16 13:05:53 -0400

    Update package-install.R

--- .github/shiny-workflows/package-install.R
+++ .github/shiny-workflows/package-install.R
@@ -1,35 +1,56 @@
 is_linux   <- function() Sys.info()[["sysname"]] == "Linux"
 
 
-install_troublesome_pkgs <- function(pkgs) {
+install_troublesome_pkgs <- function(pkg_infos) {
   if (!requireNamespace("pak")) {
     install.packages("pak", repos = sprintf("https://r-lib.github.io/p/pak/stable/%s/%s/%s", .Platform$pkgType, R.Version()$os, R.Version()$arch))
   }
 
-  # Always install system deps
-  pak::pkg_system_requirements(pkgs, execute = TRUE)
-
   # Install missing pkgs
   installed_packages <- as.data.frame(installed.packages(), stringsAsFactors = FALSE)$Package
-  pkgs_to_install <- pkgs[!(pkgs %in% installed_packages)]
-  if (length(pkgs_to_install) > 0) {
-    message("Installing packages: ", paste0(pkgs_to_install, collapse = ", "))
-    install.packages(pkgs_to_install)
+  for (pkg_info in pkg_infos) {
+    pkg_name <- pkg_info$name
+    pkg_version <- pkg_info$version
+    if (!(pkg_name %in% installed_packages)) {
+      message("Installing package: ", pkg_name)
+      pak::pkg_system_requirements(pkg_name, execute = TRUE)
+      if (is.null(pkg_version)) {
+        install.packages(pkg_name)
+      } else {
+        if (!requireNamespace("remotes")) {
+          install.packages("remotes")
+        }
+        remotes::install_version(pkg_name, pkg_version)
+      }
+    }
   }
 }
 
 if (is_linux()) {
   switch(as.character(getRversion()),
     "3.6.3" = {
-      install_troublesome_pkgs(c("rjson"))
+      install_troublesome_pkgs(
+        list(
+          list(name = "rjson", version = "0.2.20")
+        )
+      )
     },
     "3.5.3" = {
-      install_troublesome_pkgs(c("rjson", "radiant"))
+      install_troublesome_pkgs(
+        list(
+          list(name = "rjson", version = "0.2.20"),
+          list(name = "radiant", version = "1.3.4")
+        )
+      )
       # These packages do not like to be installed on earlier R versions
     },
     {
       if (grepl("^4\\.2", getRversion())) {
-        install_troublesome_pkgs(c("XML"))
+        install_troublesome_pkgs(
+          list(
+            list(name = "XML", version = NULL)
+          )
+        )
       }
     }
   )
