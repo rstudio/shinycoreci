8d03a7676e299a2feda7f678cb7a71832451a818
Barret Schloerke <schloerke@gmail.com>
2022-05-18 16:50:42 -0400

    Update package-install.R

--- .github/shiny-workflows/package-install.R
+++ .github/shiny-workflows/package-install.R
@@ -1,3 +1,4 @@
 if (!requireNamespace("digest", quietly = TRUE)) {
+  # Need for url remotes. See https://github.com/r-lib/actions/issues/562#issuecomment-1129088041
   install.packages("digest", repos = "http://cran.us.r-project.org")
 }
