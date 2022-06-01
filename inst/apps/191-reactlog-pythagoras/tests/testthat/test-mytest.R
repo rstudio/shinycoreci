c1802d798108a43006dbce93680767d4cd097693
Barret Schloerke <schloerke@gmail.com>
2022-06-01 10:54:25 -0400

    Apply same changes to 191-reactlog-pythagorus

--- inst/apps/191-reactlog-pythagoras/tests/testthat/test-mytest.R
+++ inst/apps/191-reactlog-pythagoras/tests/testthat/test-mytest.R
@@ -2,17 +2,18 @@ library(shinytest2)
 
 test_that("Migrated shinytest test: mytest.R", {
   app <- AppDriver$new(variant = shinytest2::platform_variant(),
-    seed = 54322)
+    seed = 100)
 
-  app$set_inputs(`reactlog_module-refresh` = "click")
-  Sys.sleep(4) # wait for reactlog to settle
-  app$expect_values()
-  app$expect_screenshot()
+  refresh_and_expect <- function() {
+    app$set_inputs(`reactlog_module-refresh` = "click")
+    Sys.sleep(3) # wait for reactlog to settle
+    app$expect_values()
+    app$expect_screenshot()
+  }
+
+  refresh_and_expect()
 
   app$set_inputs(a = 5, b = 12)
 
-  app$set_inputs(`reactlog_module-refresh` = "click")
-  Sys.sleep(4) # wait for reactlog to settle
-  app$expect_values()
-  app$expect_screenshot()
+  refresh_and_expect()
 })
