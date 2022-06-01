eebcccbad9b29d3d05de1c515295a108b518a30c
Barret Schloerke <schloerke@gmail.com>
2022-06-01 10:35:08 -0400

    Reduce number of images taken

--- inst/apps/190-reactlog-module-ex1/tests/testthat/test-mytest.R
+++ inst/apps/190-reactlog-module-ex1/tests/testthat/test-mytest.R
@@ -12,26 +12,17 @@ test_that("Migrated shinytest test: mytest.R", {
   app <- AppDriver$new(variant = shinytest2::platform_variant(),
     seed = 54322)
 
-  app$set_inputs(`reactlog_module-refresh` = "click")
-  Sys.sleep(4) # wait for reactlog to settle
-  app$expect_values()
-  app$expect_screenshot()
+  refresh_and_expect <- function() {
+    app$set_inputs(`reactlog_module-refresh` = "click")
+    Sys.sleep(1.5) # wait for reactlog to settle
+    app$expect_values()
+    app$expect_screenshot()
+  }
 
-  app$set_inputs(`reactlog_module-refresh` = "click")
-  Sys.sleep(4) # wait for reactlog to settle
-  app$expect_values()
-  app$expect_screenshot()
+  refresh_and_expect()
 
   app$set_inputs(obs = 9)
   app$set_inputs(obs = 8)
 
-  app$set_inputs(`reactlog_module-refresh` = "click")
-  Sys.sleep(4) # wait for reactlog to settle
-  app$expect_values()
-  app$expect_screenshot()
-
-  app$set_inputs(`reactlog_module-refresh` = "click")
-  Sys.sleep(4) # wait for reactlog to settle
-  app$expect_values()
-  app$expect_screenshot()
+  refresh_and_expect()
 })
