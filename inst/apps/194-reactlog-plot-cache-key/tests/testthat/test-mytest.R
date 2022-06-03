a56fe466fdbae1011c5cf2cdf51668bf8feebbd5
Barret Schloerke <schloerke@gmail.com>
2022-06-03 17:15:45 -0400

    Copy cadence from 190-193 app tests

--- inst/apps/194-reactlog-plot-cache-key/tests/testthat/test-mytest.R
+++ inst/apps/194-reactlog-plot-cache-key/tests/testthat/test-mytest.R
@@ -4,32 +4,24 @@ test_that("Migrated shinytest test: mytest.R", {
   app <- AppDriver$new(variant = shinytest2::platform_variant(),
     seed = 100, shiny_args = list(display.mode = "normal"))
 
-  app$set_inputs(`reactlog_module-refresh` = "click")
-  Sys.sleep(4) # wait for reactlog to settle
-  app$expect_values()
-  app$expect_screenshot()
+  refresh_and_expect <- function() {
+    app$set_inputs(`reactlog_module-refresh` = "click")
+    Sys.sleep(3) # wait for reactlog to settle
+    app$expect_values()
+    # app$expect_screenshot() # Not consistent. Disabling
+  }
+
+  refresh_and_expect()
 
   app$set_inputs(n = 250)
-  app$set_inputs(`reactlog_module-refresh` = "click")
-  Sys.sleep(4) # wait for reactlog to settle
-  app$expect_values()
-  app$expect_screenshot()
+  refresh_and_expect()
 
   app$set_inputs(n = 100)
-  app$set_inputs(`reactlog_module-refresh` = "click")
-  Sys.sleep(4) # wait for reactlog to settle
-  app$expect_values()
-  app$expect_screenshot()
+  refresh_and_expect()
 
   app$set_inputs(newdata = "click")
-  app$set_inputs(`reactlog_module-refresh` = "click")
-  Sys.sleep(4) # wait for reactlog to settle
-  app$expect_values()
-  app$expect_screenshot()
+  refresh_and_expect()
 
   app$set_inputs(n = 300)
-  app$set_inputs(`reactlog_module-refresh` = "click")
-  Sys.sleep(4) # wait for reactlog to settle
-  app$expect_values()
-  app$expect_screenshot()
+  refresh_and_expect()
 })
