d4a25b63c67f5c8e146dd5c2d52aa93691a74ced
Carson Sievert <cpsievert1@gmail.com>
2022-06-24 16:36:29 -0500

    Skip IE on 187 (#122)

--- inst/apps/187-navbar-collapse/tests/testthat/test-shinyjster.R
+++ inst/apps/187-navbar-collapse/tests/testthat/test-shinyjster.R
@@ -1,9 +1,10 @@
 library(bslib)
 
-shinyjster::testthat_shinyjster("No theme", dimensions = "550x700")
+browsers <- c("chrome", "firefox", "edge")
+shinyjster::testthat_shinyjster("No theme", dimensions = "550x700", browsers = browsers)
 
 local({
   old_theme <- bs_global_set(bs_theme())
   on.exit(bs_global_set(old_theme))
-  shinyjster::testthat_shinyjster("bs4", dimensions = "550x700")
+  shinyjster::testthat_shinyjster("bs4", dimensions = "550x700", browsers = browsers)
 })
