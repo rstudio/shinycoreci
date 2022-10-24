12fd77acfd95f486b0f3870c95b31d346ce9586a
Barret Schloerke <schloerke@gmail.com>
2022-10-24 13:59:45 -0400

    Test to make sure no warnings appear with legacy icon names

--- inst/apps/138-icon-fontawesome/tests/testthat/test-mytest.R
+++ inst/apps/138-icon-fontawesome/tests/testthat/test-mytest.R
@@ -6,4 +6,22 @@ test_that("Migrated shinytest test: mytest.R", {
 
   app$expect_values()
   app$expect_screenshot()
+
+  # Make sure no warnings about `Font Awesome` were thrown
+  # See https://github.com/rstudio/fontawesome/pull/96
+  # Ex warning:
+  #> The `name` provided ('eur') is deprecated in Font Awesome 6:
+  #> * please consider using 'euro-sign' or 'fas fa-euro-sign' instead
+  #> * use the `verify_fa = FALSE` to deactivate these messages
+  #> Running application in test mode.
+  logs <- format(app$get_logs())
+  expect_false(grepl("Font Awesome", logs))
+})
+
+test_that("Font Awesome Icons do not display warnings for historical icons", {
+  # From within this app
+  expect_warning(shiny::icon("eur"), NA)
+
+  # https://github.com/rstudio/reactlog/pull/87
+  expect_warning(shiny::icon("refresh"), NA)
 })
