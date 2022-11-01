39ec4b6ed071a2d89722040815ea289762e42266
Barret Schloerke <schloerke@gmail.com>
2022-11-01 16:13:51 -0400

    Fix timeout bug in test for 133

--- inst/apps/133-async-hold-inputs/tests/testthat/test-mytest.R
+++ inst/apps/133-async-hold-inputs/tests/testthat/test-mytest.R
@@ -1,18 +1,21 @@
 library(shinytest2)
 
 test_that("Migrated shinytest test: mytest.R", {
-  app <- AppDriver$new(variant = shinytest2::platform_variant())
+  app <- AppDriver$new(variant = shinytest2::platform_variant(), wait = FALSE)
 
   app$set_inputs(go = "click", wait_ = FALSE)
   app$set_inputs(choice = "b", wait_ = FALSE)
   app$set_inputs(choice = "c", wait_ = FALSE)
+  # Expect values before the first click calculation returns
   app$expect_values()
 
   ## Commenting because the screenshot may or may not get the right
   ## timing with the progress notification in the corner
   # app$expect_screenshot()
 
-  app$wait_for_idle(duration = 1500)
+  # Must wait at least 3 seconds for each click to return
+  # with a 2 second buffer on top of that
+  app$wait_for_idle(timeout = (3 * 3 + 2) * 1000)
 
   app$expect_values()
   app$expect_screenshot()
