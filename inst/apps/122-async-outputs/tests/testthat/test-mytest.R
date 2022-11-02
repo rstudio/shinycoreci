eb7a521bf3e81a92402cfeb6510a78c3f506b22c
Barret Schloerke <schloerke@gmail.com>
2022-11-02 11:26:58 -0400

    Fix missing key arg name

--- inst/apps/122-async-outputs/tests/testthat/test-mytest.R
+++ inst/apps/122-async-outputs/tests/testthat/test-mytest.R
@@ -5,7 +5,7 @@ test_that("Migrated shinytest test: mytest.R", {
     load_timeout = 15000, seed = 100, shiny_args = list(display.mode = "normal"))
 
   # Wait until an async value is available
-  app$wait_for_value("printa")
+  app$wait_for_value(output = "printa")
 
   app$expect_values()
   app$expect_screenshot()
