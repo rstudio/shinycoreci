a3e773f7a33c306e6af67281c89271f6cb42bc16
Barret Schloerke <schloerke@gmail.com>
2022-11-03 12:33:52 -0400

    Require systemfonts v0.3 for `systemfonts::register_font()`

--- inst/apps/183-report-cairo/tests/testthat/test-mytest.R
+++ inst/apps/183-report-cairo/tests/testthat/test-mytest.R
@@ -2,6 +2,7 @@ library(shinytest2)
 
 test_that("Migrated shinytest test: mytest.R", {
   skip_if_not_installed("ragg", "0.2")
+  skip_if_not_installed("systemfonts", "0.3") # systemfonts::register_font
 
   app <- AppDriver$new(variant = shinytest2::platform_variant())
 
