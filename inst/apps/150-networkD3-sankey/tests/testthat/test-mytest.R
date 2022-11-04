d71b52fe794ce141524bdd581154eda1adc4c73b
Barret Schloerke <schloerke@gmail.com>
2022-11-04 10:53:03 -0400

    Accept snaps

--- inst/apps/150-networkD3-sankey/tests/testthat/test-mytest.R
+++ inst/apps/150-networkD3-sankey/tests/testthat/test-mytest.R
@@ -15,5 +15,5 @@ test_that("Migrated shinytest test: mytest.R", {
   app$set_inputs(._bookmark_ = "click")
   Sys.sleep(1) # wait for modal
   app$expect_values()
-  app$expect_screenshot()
+  app$expect_screenshot(threshold = 2)
 })
