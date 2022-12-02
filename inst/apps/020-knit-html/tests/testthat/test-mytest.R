0c047d877d2e2a865b4f30fe375d4a29470e99cc
Barret Schloerke <schloerke@gmail.com>
2022-12-02 11:57:36 -0500

    Top errors from showing up in R 3.5 for app 020

--- inst/apps/020-knit-html/tests/testthat/test-mytest.R
+++ inst/apps/020-knit-html/tests/testthat/test-mytest.R
@@ -1,6 +1,12 @@
 library(shinytest2)
 
 test_that("Migrated shinytest test: mytest.R", {
+
+  skip_if(
+    getRversion() < "3.6",
+    "`rmarkdown::mark_html()` has difficulties being found in early R versions."
+  )
+
   app <- AppDriver$new(variant = shinytest2::platform_variant(),
     seed = 100, shiny_args = list(display.mode = "normal"))
 
