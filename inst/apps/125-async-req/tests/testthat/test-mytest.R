961f8ba3e21b66103dbc2f997d38fe392024cfaf
Barret Schloerke <schloerke@gmail.com>
2022-12-02 11:52:02 -0500

    Make 125 more robust by waiting for init output value

--- inst/apps/125-async-req/tests/testthat/test-mytest.R
+++ inst/apps/125-async-req/tests/testthat/test-mytest.R
@@ -3,9 +3,11 @@ library(shinytest2)
 test_that("Migrated shinytest test: mytest.R", {
   app <- AppDriver$new(variant = shinytest2::platform_variant())
 
-  app$wait_for_idle()
+  app$wait_for_value(output = "validateNeed_async")
+
   app$expect_values()
   app$expect_screenshot()
+
   app$set_inputs(boom = "click")
   app$expect_values()
   app$expect_screenshot()
