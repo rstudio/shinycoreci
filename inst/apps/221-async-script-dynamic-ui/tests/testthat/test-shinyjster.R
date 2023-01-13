0b5b8a9edf07fc7aa62a3c9a205d3aa946c19733
Winston Chang <winston@stdout.org>
2023-01-13 11:35:48 -0600

    Don't test on R <=3.5

--- inst/apps/221-async-script-dynamic-ui/tests/testthat/test-shinyjster.R
+++ inst/apps/221-async-script-dynamic-ui/tests/testthat/test-shinyjster.R
@@ -1,1 +1,5 @@
-shinyjster::testthat_shinyjster()
+# R<=3.5 doesn't support removeSource() being called on an expression, and
+# throws an error.
+if (getRversion() >= "3.6") {
+    shinyjster::testthat_shinyjster()
+}
