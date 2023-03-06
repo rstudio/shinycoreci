1a3272a270063c342542780848324bdc29099dd3
Carson <cpsievert1@gmail.com>
2023-03-06 15:50:06 -0600

    Make sure the set_inputs() calls complete in order

--- inst/apps/306-accordion-add-remove/tests/testthat/test-shinytest2.R
+++ inst/apps/306-accordion-add-remove/tests/testthat/test-shinytest2.R
@@ -11,45 +11,40 @@ test_that("{shinytest2} recording: accordion-select", {
     options = list(bslib.precompiled = FALSE)
   )
 
-  expect_screenshot <- function(..., duration = 500, viewport = TRUE, threshold = 3) {
-    app$wait_for_idle(duration = duration)
-    args <- rlang::list2(..., threshold = threshold)
-    if (viewport) {
-      rect <- c(x = 0, y = 0, width = width, height = height)
-      new_args <- list(screenshot_args = list(cliprect = rect))
-      args <- modifyList(new_args, args)
-    }
-    do.call(app$expect_screenshot, args)
+  # Make sure the set_input() calls complete in order
+  set_inputs <- function(...) {
+    app$set_inputs(...)
+    app$wait_for_idle()
   }
 
   # Test accordion_panel_set()
-  app$set_inputs(selected = c("A", "D"))
-  app$set_inputs(selected = c("A", "D", "H"))
-  expect_screenshot()
+  set_inputs(selected = c("A", "D"))
+  set_inputs(selected = c("A", "D", "H"))
+  app$expect_screenshot()
 
   # Test accordion_panel_remove()
-  app$set_inputs(displayed = c("D", "F"))
+  set_inputs(displayed = c("D", "F"))
   # Test accordion_panel_insert()
-  app$set_inputs(displayed = c("A", "D", "F"))
-  app$set_inputs(displayed = c("A", "D", "F", "Z"))
+  set_inputs(displayed = c("A", "D", "F"))
+  set_inputs(displayed = c("A", "D", "F", "Z"))
   # Test accordion_panel_insert() + accordion_panel_open()
-  app$set_inputs(open_on_insert = TRUE)
-  app$set_inputs(displayed = c("A", "D", "F", "J", "Z"))
-  app$set_inputs(displayed = c("A", "D", "F", "J", "K", "Z"))
-  expect_screenshot()
+  set_inputs(open_on_insert = TRUE)
+  set_inputs(displayed = c("A", "D", "F", "J", "Z"))
+  set_inputs(displayed = c("A", "D", "F", "J", "K", "Z"))
+  app$expect_screenshot()
 
   # redo tests with accordion(autoclose = TRUE)
-  app$set_inputs(open_on_insert = FALSE)
-  app$set_inputs(multiple = FALSE)
+  set_inputs(open_on_insert = FALSE)
+  set_inputs(multiple = FALSE)
 
   # Last one (D) should be selected
-  app$set_inputs(selected = "B")
-  app$set_inputs(selected = c("C", "D"))
-  expect_screenshot()
+  set_inputs(selected = "B")
+  set_inputs(selected = c("C", "D"))
+  app$expect_screenshot()
 
-  app$set_inputs(displayed = c("A", "D", "F", "Z"))
-  app$set_inputs(open_on_insert = TRUE)
-  app$set_inputs(displayed = c("A", "D", "F", "J", "Z"))
-  app$set_inputs(displayed = c("A", "D", "F", "J", "K", "Z"))
-  expect_screenshot()
+  set_inputs(displayed = c("A", "D", "F", "Z"))
+  set_inputs(open_on_insert = TRUE)
+  set_inputs(displayed = c("A", "D", "F", "J", "Z"))
+  set_inputs(displayed = c("A", "D", "F", "J", "K", "Z"))
+  app$expect_screenshot()
 })
