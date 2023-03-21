ee8ad9a54969ea638fa9050308fd592db6bf5550
Garrick Aden-Buie <garrick@adenbuie.com>
2023-03-21 11:56:16 -0400

    Don't use for-loops to iterate over boxe state

--- inst/apps/309-flexdashboard-tabs-navs/tests/testthat/test-shinytest2.R
+++ inst/apps/309-flexdashboard-tabs-navs/tests/testthat/test-shinytest2.R
@@ -35,22 +35,28 @@ for (bs_version in 3:5) {
     app$wait_for_idle()
     app$wait_for_js(is_test_element_visible("Page 1"))
 
+    # ---- Page 1 ----
+    # Page 1 and its boxes are visible
     expect_test_element_visible(app, "Page 1")
+    expect_test_element_visible(app, "Box 1-1")
+    expect_test_element_visible(app, "Box 1-2")
+
+    # Check tab state on Page 1 (first tabs are visible)
     expect_test_element_visible(app, "Tab 1-1a")
     expect_test_element_visible(app, "Tab 1-2a")
-    for (tab in c("Tab 1-1a", "Tab 1-2a")) {
-      expect_test_element_visible(app, tab)
-    }
+    # second tabs are hidden
+    expect_test_element_hidden(app, "Tab 1-1b")
+    expect_test_element_hidden(app, "Tab 1-2b")
 
+    # Pages 2 and 3 and their elements are hidden
     expect_test_element_hidden(app, "Page 2")
+    expect_test_element_hidden(app, "Box 2-1")
+    expect_test_element_hidden(app, "Box 2-2")
     expect_test_element_hidden(app, "Page 3")
-    for (box in c("Box 2-1", "Box 2-2", "Box 3-1", "Box 3-2")) {
-      expect_test_element_hidden(app, box)
-    }
-    for (tab in c("Tab 1-1b", "Tab 1-2b")) {
-      expect_test_element_hidden(app, tab)
-    }
+    expect_test_element_hidden(app, "Box 3-1")
+    expect_test_element_hidden(app, "Box 3-2")
 
+    # ---- Page 1: Change Tabs ----
     # activate second tabs and check that visibility has switched
     app$
       click(selector = '[data-test-id="Page 1"] .nav-tabs [href$="tab-1-1b"]')$
@@ -60,46 +66,52 @@ for (bs_version in 3:5) {
       click(selector = '[data-test-id="Page 1"] .nav-tabs [href$="tab-1-2b"]')$
       wait_for_js(is_test_element_visible("Tab 1-2b"))
 
-    for (tab in c("Tab 1-1a", "Tab 1-2a")) {
-      expect_test_element_hidden(app, tab)
-    }
-    for (tab in c("Tab 1-1b", "Tab 1-2b")) {
-      expect_test_element_visible(app, tab)
-    }
+    # now first tabs are hidden
+    expect_test_element_hidden(app, "Tab 1-1a")
+    expect_test_element_hidden(app, "Tab 1-2a")
+    # second tabs are visible
+    expect_test_element_visible(app, "Tab 1-1b")
+    expect_test_element_visible(app, "Tab 1-2b")
 
-    # Go to page 2
+    # ---- Page 2 ----
     app$
       click(selector = ".nav .dropdown .dropdown-toggle")$
       wait_for_js(is_element_visible(".nav .dropdown .dropdown-menu"))$
       click(selector = '.nav .dropdown-item[href$="page-2"]')$
       wait_for_js(is_test_element_visible("Page 2"))
 
+    # Page 2 is visible
     expect_test_element_visible(app, "Page 2")
     expect_test_element_visible(app, "Box 2-1")
     expect_test_element_visible(app, "Box 2-2")
 
+    # Pages 1 and 3 and their elements are hidden
     expect_test_element_hidden(app, "Page 1")
+    expect_test_element_hidden(app, "Box 1-1")
+    expect_test_element_hidden(app, "Box 1-2")
     expect_test_element_hidden(app, "Page 3")
-    for (box in c("Box 1-1", "Box 1-2", "Box 3-1", "Box 3-2")) {
-      expect_test_element_hidden(app, box)
-    }
+    expect_test_element_hidden(app, "Box 3-1")
+    expect_test_element_hidden(app, "Box 3-2")
 
-    # Go to page 3
+    # ---- Page 3 ----
     app$
       click(selector = ".nav .dropdown .dropdown-toggle")$
       wait_for_js(is_element_visible(".nav .dropdown .dropdown-menu"))$
       click(selector = '.nav .dropdown-item[href$="page-3"]')$
       wait_for_js(is_test_element_visible("Page 3"))
 
+    # Page 3 is visible
     expect_test_element_visible(app, "Page 3")
     expect_test_element_visible(app, "Box 3-1")
     expect_test_element_visible(app, "Box 3-2")
 
+    # Pages 1 and 2 and their elements are hidden
     expect_test_element_hidden(app, "Page 1")
+    expect_test_element_hidden(app, "Box 1-1")
+    expect_test_element_hidden(app, "Box 1-2")
     expect_test_element_hidden(app, "Page 2")
-    for (box in c("Box 1-1", "Box 1-2", "Box 2-1", "Box 2-2")) {
-      expect_test_element_hidden(app, box)
-    }
+    expect_test_element_hidden(app, "Box 2-1")
+    expect_test_element_hidden(app, "Box 2-2")
 
     app$stop()
   })
