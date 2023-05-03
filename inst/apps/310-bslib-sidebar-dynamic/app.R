49b4ae760b695b9be8808da25766516c3cf207a4
Garrick Aden-Buie <garrick@adenbuie.com>
2023-05-03 15:39:31 -0400

    310: Sidebar tests with dynamic UI

---
+++ inst/apps/310-bslib-sidebar-dynamic/app.R
@@ -0,0 +1,164 @@
+
+library(shiny)
+# pak::pak("rstudio/bslib#555")
+library(bslib)
+
+INCLUDE_INITIAL_SIDEBAR <- Sys.getenv("INCLUDE_INITIAL_SIDEBAR", FALSE)
+
+color_pairs <- list(
+  list(dark = "#1A2A6C", light = "#AED9E0"),
+  list(dark = "#800020", light = "#F6DFD7"),
+  list(dark = "#4B0082", light = "#E6E6FA"),
+  list(dark = "#006D5B", light = "#A2D5C6")
+)
+
+adjectives <- c(
+  "charming", "cuddly", "elegant", "fierce", "graceful",
+  "majestic", "playful", "quirky", "silly", "witty"
+)
+animals <- c(
+  "elephant", "giraffe", "jaguar", "koala", "lemur",
+  "otter", "panda", "panther", "penguin", "zebra"
+)
+
+nested_sidebar <- function(idx = 0L) {
+  colors <- color_pairs[[idx %% length(color_pairs) + 1]]
+  open <- c("open", "closed", "desktop")[(idx - 1) %% 3 + 1]
+
+  select_adjective <- function() {
+    selectInput(
+      paste0("adjective_", idx),
+      "Adjective",
+      choices = adjectives,
+      selected = adjectives[idx %% length(adjectives) + 1]
+    )
+  }
+
+  select_animal <- function() {
+    selectInput(
+      paste0("animal_", idx),
+      "Animal",
+      choices = animals,
+      selected = animals[idx %% length(animals) + 1]
+    )
+  }
+
+  layout_sidebar(
+    id = paste0("main_outer_", idx),
+    sidebar = sidebar(
+      "Outer Sidebar",
+      id = paste0("sidebar_outer_", idx),
+      width = 150,
+      bg = colors$dark,
+      open = open,
+      max_height_mobile = "300px",
+      select_adjective()
+    ),
+    height = 300,
+    class = "p-0",
+    fillable = TRUE,
+    layout_sidebar(
+      id = paste0("main_inner_", idx),
+      sidebar = sidebar(
+        "Inner Sidebar",
+        id = paste0("sidebar_inner_", idx),
+        width = 150,
+        bg = colors$light,
+        open = open,
+        select_animal()
+      ),
+      border = FALSE,
+      border_radius = FALSE,
+      h2("Sidebar Layout", idx),
+      uiOutput(paste0("ui_content_", idx)),
+    )
+  ) |>
+  tagAppendAttributes(class = "mb-4", id = paste0("layout_", idx))
+}
+
+ui <- page_fixed(
+  h1("Dynamic Sidebars"),
+  tags$head(tags$title("bslib | Tests | Dynamic Sidebars")),
+  p(
+    "Test dynamically added sidebars.",
+    "Each new layout is a nested layout with two sidebars.",
+    "The sidebar collapse toggles should not overlap when collapsed",
+    "and if you add the sidebar while in mobile screen width the sidebars",
+    "will be closed when added."
+  ),
+  layout_column_wrap(
+    width = 500,
+    id = "sidebar-here",
+    if (INCLUDE_INITIAL_SIDEBAR) nested_sidebar()
+  ),
+  div(
+    class = "my-2",
+    actionButton("add_sidebar", "Add sidebar layout"),
+    actionButton("remove_sidebar", "Remove sidebar layout")
+  ),
+  div(
+    class = "my-2",
+    actionButton("show_all", "Show all"),
+    actionButton("toggle_last_inner", "Toggle last inner"),
+    actionButton("toggle_last_outer", "Toggle last outer")
+  )
+)
+
+server <- function(input, output, session) {
+  idx <- 0L
+  has_sidebar <- INCLUDE_INITIAL_SIDEBAR
+
+  output_nested_sidebar <- function(idx) {
+    output_id <- paste0("ui_content_", idx)
+    adjective_id <- paste0("adjective_", idx)
+    animal_id <- paste0("animal_", idx)
+
+    output[[output_id]] <- renderUI({
+      p(sprintf("Hello, %s %s!", input[[adjective_id]], input[[animal_id]]))
+    })
+  }
+
+  if (INCLUDE_INITIAL_SIDEBAR) {
+    observe({
+      isolate(output_nested_sidebar(0))
+    })
+  }
+
+  observeEvent(input$add_sidebar, {
+    if (idx == 0) has_sidebar <<- TRUE
+    idx <<- idx + 1L
+
+    insertUI(
+      selector = "#sidebar-here",
+      where = "beforeEnd",
+      ui = nested_sidebar(idx)
+    )
+
+    output_nested_sidebar(idx)
+  })
+
+  observeEvent(input$remove_sidebar, {
+    removeUI(selector = "#sidebar-here > :last-child")
+  })
+
+  observeEvent(input$show_all, {
+    req(has_sidebar)
+    ids <- grep("^sidebar_", names(input), value = TRUE)
+    for (id in ids) {
+      message("opening ", id)
+      sidebar_toggle(id, open = TRUE)
+    }
+  })
+
+  observeEvent(input$toggle_last_inner, {
+    req(has_sidebar)
+    sidebar_toggle(paste0("sidebar_inner_", idx))
+  })
+
+  observeEvent(input$toggle_last_outer, {
+    req(has_sidebar)
+    sidebar_toggle(paste0("sidebar_outer_", idx))
+  })
+}
+
+shinyApp(ui, server)
---
+++ inst/apps/310-bslib-sidebar-dynamic/tests/testthat/test-shinytest2.R
@@ -0,0 +1,410 @@
+library(shinytest2)
+
+# Only run these tests on mac + r-release
+# (To reduce the amount of screenshot diffing noise)
+release <- rversions::r_release()$version
+release <- paste0(
+  strsplit(release, ".", fixed = TRUE)[[1]][1:2],
+  collapse = "."
+)
+
+is_testing_on_ci <- identical(Sys.getenv("CI"), "true") && testthat::is_testing()
+is_mac_release <- identical(paste0("mac-", release), platform_variant())
+
+if (is_testing_on_ci && !is_mac_release) {
+  skip("Not mac + r-release")
+}
+
+key_press_factory <- function(app) {
+  brwsr <- app$get_chromote_session()
+
+  function(which = "Tab", shift = FALSE) {
+    virtual_code <- switch(
+      which,
+      Tab = 9,
+      Enter = 13,
+      Escape = 27,
+      ArrowLeft = 37,
+      ArrowUp = 38,
+      ArrowRight = 39,
+      ArrowDown = 40,
+      Backspace = 8,
+      Delete = 46,
+      Home = 36,
+      End = 35,
+      PageUp = 33,
+      PageDown = 34,
+      Space = 32
+    )
+
+    modifiers <- 0
+    if (shift) modifiers <- modifiers + 8
+    # if (command) modifiers <- modifiers + 4
+    # if (control) modifiers <- modifiers + 2
+    # if (alt) modifiers <- modifiers + 1
+
+    events <-
+      brwsr$Input$dispatchKeyEvent(
+        "rawKeyDown",
+        windowsVirtualKeyCode = virtual_code,
+        code = which,
+        key = which,
+        modifiers = modifiers,
+        wait_ = FALSE
+      )$then(
+        brwsr$Input$dispatchKeyEvent(
+          "keyUp",
+          windowsVirtualKeyCode = virtual_code,
+          code = which,
+          key = which,
+          modifiers = modifiers,
+          wait_ = FALSE
+        )
+      )
+
+    brwsr$wait_for(events)
+
+    invisible(app)
+  }
+}
+
+expect_sidebar_hidden_factory <- function(app) {
+  function(id, which = c("inner", "outer")) {
+    state <- app$get_js(js_sidebar_state(id = id, which = which))
+    expect_true("sidebar-collapsed" %in% state$layout_classes)
+    expect_equal(state$content_display, "none")
+    expect_true(state$sidebar_hidden)
+  }
+}
+
+expect_sidebar_shown_factory <- function(app) {
+  function(id, which = c("inner", "outer")) {
+    state <- app$get_js(js_sidebar_state(id = id, which = which))
+    expect_false("sidebar-collapsed" %in% state$layout_classes)
+    expect_false(identical(state$content_display, "none"))
+    expect_false(state$sidebar_hidden)
+  }
+}
+
+js_sidebar_transition_complete <- function(id, which = c("inner", "outer")) {
+  which <- match.arg(which)
+  selector <- sprintf("#sidebar_%s_%s", which, id)
+  sprintf(
+    "!document.querySelector('%s').parentElement.classList.contains('transitioning');",
+    selector
+  )
+}
+
+js_sidebar_state <- function(id, which = c("inner", "outer")) {
+  which <- match.arg(which)
+  selector <- sprintf("#sidebar_%s_%s", which, id)
+  sprintf(
+    "(function() {
+      return {
+      layout_classes: Array.from(document.querySelector('%s').closest('.bslib-sidebar-layout').classList),
+      content_display: window.getComputedStyle(document.querySelector('%s .sidebar-content')).getPropertyValue('display'),
+      sidebar_hidden: document.querySelector('%s').hidden
+    }})();",
+    selector, selector, selector
+  )
+}
+
+test_that("310-bslib-sidebar-dynamic: dynamically added sidebars are fully functional", {
+  app <- AppDriver$new(
+    name = "310-bslib-sidebar-dynamic",
+    variant = platform_variant(),
+    height = 800,
+    width = 1200,
+    view = interactive(),
+    options = list(bslib.precompiled = FALSE),
+    expect_values_screenshot_args = FALSE
+  )
+
+  expect_sidebar_hidden <- expect_sidebar_hidden_factory(app)
+  expect_sidebar_shown <- expect_sidebar_shown_factory(app)
+
+  expect_sidebar_main_text <- function(id, pattern, ...) {
+    text <- app$get_js(sprintf("document.querySelector('#main_inner_%s').textContent", id))
+    expect_match(text, pattern, ...)
+  }
+
+  # Add first sidebar -----
+  app$
+    click("add_sidebar")$
+    wait_for_js("document.getElementById('layout_1') ? true : false")$
+    expect_values()
+
+  app$expect_screenshot(selector = "#layout_1")
+  expect_sidebar_main_text(1, "cuddly giraffe")
+
+  # First sidebar starts open = "open"
+  expect_sidebar_shown(id = 1, "inner")
+  expect_sidebar_shown(id = 1, "outer")
+
+  # Update an input in the sidebar to test that the output is updated
+  app$set_inputs(adjective_1 = "elegant")
+  expect_sidebar_main_text(1, "elegant giraffe")
+
+  # Collapse the inner sidebar
+  app$
+    click(selector = "#main_inner_1 ~ .collapse-toggle")$
+    wait_for_js(js_sidebar_transition_complete(id = 1))$
+    expect_values()
+
+  # Only inner sidebar is hidden
+  expect_sidebar_hidden(id = 1, "inner")
+  expect_sidebar_shown(id = 1, "outer")
+  app$expect_screenshot(selector = "#layout_1")
+
+  # Collapse the outer sidebar
+  app$
+    click(selector = "#main_outer_1 ~ .collapse-toggle")$
+    wait_for_js(js_sidebar_transition_complete(id = 1, "outer"))$
+    expect_values()
+
+  # both sidebars are collapsed
+  expect_sidebar_hidden(id = 1, "inner")
+  expect_sidebar_hidden(id = 1, "outer")
+  app$expect_screenshot(selector = "#layout_1")
+
+  # Expand inner sidebar
+  app$
+    click(selector = "#main_inner_1 ~ .collapse-toggle")$
+    wait_for_js(js_sidebar_transition_complete(id = 1))$
+    expect_values()
+
+  # Only inner sidebar is hidden
+  expect_sidebar_shown(id = 1, "inner")
+  expect_sidebar_hidden(id = 1, "outer")
+  app$expect_screenshot(selector = "#layout_1")
+
+  # Expand the outer sidebar
+  app$
+    click(selector = "#main_outer_1 ~ .collapse-toggle")$
+    wait_for_js(js_sidebar_transition_complete(id = 1, "outer"))$
+    expect_values()
+
+  # both sidebars are shown
+  expect_sidebar_shown(id = 1, "inner")
+  expect_sidebar_shown(id = 1, "outer")
+  app$expect_screenshot(selector = "#layout_1")
+
+  # Update both inputs
+  app$set_inputs(animal_1 = "panda", adjective_1 = "silly")
+  expect_sidebar_main_text(1, "silly panda")
+
+  # Add second sidebar -----
+  app$
+    click("add_sidebar")$
+    wait_for_js("document.getElementById('layout_2') ? true : false")$
+    expect_values()
+  expect_sidebar_main_text(2, "elegant jaguar")
+
+  # both sidebars are hidden because open = "closed"
+  expect_sidebar_hidden(id = 2, "inner")
+  expect_sidebar_hidden(id = 2, "outer")
+  app$expect_screenshot(selector = "#layout_2")
+
+  # Change an input while it's hidden
+  app$set_inputs(animal_2 = "zebra")
+  expect_sidebar_main_text(2, "elegant zebra")
+
+  # Expand the outer sidebar
+  app$
+    click(selector = "#main_outer_2 ~ .collapse-toggle")$
+    wait_for_js(js_sidebar_transition_complete(id = 2, "outer"))$
+    expect_values()
+
+  # Outer sidebar is revealed
+  expect_sidebar_hidden(id = 2, "inner")
+  expect_sidebar_shown(id = 2, "outer")
+  app$expect_screenshot(selector = "#layout_2")
+
+  # Switch to mobile app size, using iPhone 13 dimensions
+  app$set_window_size(390, 844)
+
+  # Write a javascript string to scroll #layout_2 into view
+  app$
+    run_js("document.getElementById('layout_2').scrollIntoView()")$
+    expect_screenshot(selector = "#layout_2")
+
+  # Add third sidebar -----
+  app$
+    click("add_sidebar")$
+    wait_for_js("document.getElementById('layout_3') ? true : false")$
+    run_js("document.getElementById('layout_3').scrollIntoView()")$
+    # sidebar is closed immediately upon adding when open = "desktop"
+    wait_for_js(js_sidebar_transition_complete(id = 3))$
+    expect_values()
+
+  expect_sidebar_main_text(3, "fierce koala")
+
+  # both sidebars are hidden because open = "closed"
+  expect_sidebar_hidden(id = 3, "inner")
+  expect_sidebar_hidden(id = 3, "outer")
+  app$expect_screenshot(selector = "#layout_3")
+
+  # reveal inner sidebar
+  app$
+    click(selector = "#main_inner_3 ~ .collapse-toggle")$
+    wait_for_js(js_sidebar_transition_complete(id = 3, "inner"))$
+    run_js("document.getElementById('add_sidebar').scrollIntoView()")$
+    expect_values()
+
+  # inner sidebar is revealed
+  expect_sidebar_shown(id = 3, "inner")
+  expect_sidebar_hidden(id = 3, "outer")
+
+  # change the input value
+  app$set_inputs(animal_3 = "lemur")
+  expect_sidebar_main_text(3, "fierce lemur")
+  app$expect_screenshot(selector = "#layout_3")
+
+  # swap expanded sidebars
+  app$
+    click(selector = "#main_inner_3 ~ .collapse-toggle")$
+    click(selector = "#main_outer_3 ~ .collapse-toggle")$
+    wait_for_js(js_sidebar_transition_complete(id = 3, "outer"))$
+    expect_values()
+
+  # outer sidebar is revealed
+  expect_sidebar_hidden(id = 3, "inner")
+  expect_sidebar_shown(id = 3, "outer")
+
+  # change the input value
+  app$set_inputs(adjective_3 = "quirky")
+  expect_sidebar_main_text(3, "quirky lemur")
+  app$expect_screenshot(selector = "#layout_3")
+})
+
+test_that("310-bslib-sidebar-dynamic: test all sidebar toggling methods", {
+  withr::local_envvar(list(INCLUDE_INITIAL_SIDEBAR = TRUE))
+
+  app <- AppDriver$new(
+    name = "310-bslib-sidebar-toggle-methods",
+    variant = platform_variant(),
+    height = 800,
+    width = 1200,
+    view = interactive(),
+    options = list(bslib.precompiled = FALSE),
+    expect_values_screenshot_args = FALSE
+  )
+
+  expect_sidebar_hidden <- expect_sidebar_hidden_factory(app)
+  expect_sidebar_shown <- expect_sidebar_shown_factory(app)
+  # use chrome dev tools to send a tab keypress to the body
+  key_press <- key_press_factory(app)
+
+  # Let the output in the sidebar main panel be tab focusable
+  app$run_js("document.getElementById('ui_content_0').setAttribute('tabindex', '0')")
+
+  # First tab press enters the main content before sidebars
+  key_press("Tab")
+  expect_equal(
+    app$get_js("document.activeElement.id"),
+    "ui_content_0"
+  )
+
+  # Next tab enters the input in the inner sidebar
+  key_press("Tab")
+  expect_equal(
+    app$get_js("document.activeElement.id"),
+    "animal_0-selectized"
+  )
+
+  # Next tab focuses the inner sidebar collapse toggle
+  key_press("Tab")
+  expect_equal(
+    app$get_js("document.activeElement.getAttribute('aria-controls')"),
+    "sidebar_inner_0"
+  )
+
+  # Clicking this toggle hides the inner sidebar (note we can't directly test
+  # the enter/space event handlers because headless chrome uses mobile emulation).
+  app$
+    click(selector = ":focus")$
+    wait_for_js(js_sidebar_transition_complete(id = 0, "inner"))$
+    expect_values()
+
+  expect_sidebar_hidden(id = 0, "inner")
+  expect_sidebar_shown(id = 0, "outer")
+
+  # Next tab focuses the input in the outer sidebar
+  key_press("Tab")
+  expect_equal(
+    app$get_js("document.activeElement.id"),
+    "adjective_0-selectized"
+  )
+
+  # Next tab focuses the outer sidebar collapse toggle
+  key_press("Tab")
+  expect_equal(
+    app$get_js("document.activeElement.getAttribute('aria-controls')"),
+    "sidebar_outer_0"
+  )
+
+  # Clicking this toggle hides the outer sidebar
+  app$
+    click(selector = ":focus")$
+    wait_for_js(js_sidebar_transition_complete(id = 0, "outer"))$
+    expect_values()
+
+  expect_sidebar_hidden(id = 0, "inner")
+  expect_sidebar_hidden(id = 0, "outer")
+
+  # Next tab focuses on next input in the document
+  key_press("Tab")
+  expect_equal(
+    app$get_js("document.activeElement.id"),
+    "add_sidebar"
+  )
+
+  # Tabbing back into sidebar moves focus to outer toggle button
+  key_press("Tab", shift = TRUE)
+  expect_equal(
+    app$get_js("document.activeElement.getAttribute('aria-controls')"),
+    "sidebar_outer_0"
+  )
+
+  # Then to the inner toggle button
+  key_press("Tab", shift = TRUE)
+  expect_equal(
+    app$get_js("document.activeElement.getAttribute('aria-controls')"),
+    "sidebar_inner_0"
+  )
+
+  # Then into the sidebar main area
+  key_press("Tab", shift = TRUE)
+  expect_equal(
+    app$get_js("document.activeElement.id"),
+    "ui_content_0"
+  )
+
+  # Trigger server-side expansion of all sidebars
+  app$
+    click("show_all")$
+    wait_for_js(js_sidebar_transition_complete(id = 0, "inner"))$
+    wait_for_js(js_sidebar_transition_complete(id = 0, "outer"))$
+    expect_values()
+
+  expect_sidebar_shown(id = 0, "inner")
+  expect_sidebar_shown(id = 0, "outer")
+
+  # Trigger server-side collapse of inner sidebar
+  app$
+    click("toggle_last_inner")$
+    wait_for_js(js_sidebar_transition_complete(id = 0, "inner"))$
+    expect_values()
+
+  expect_sidebar_hidden(id = 0, "inner")
+  expect_sidebar_shown(id = 0, "outer")
+
+  # Trigger server-side collapse of outer sidebar
+  app$
+    click("toggle_last_outer")$
+    wait_for_js(js_sidebar_transition_complete(id = 0, "outer"))$
+    expect_values()
+
+  expect_sidebar_hidden(id = 0, "inner")
+  expect_sidebar_hidden(id = 0, "outer")
+})
