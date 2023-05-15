0383e2e436f8f4b840185d7af26e8b7403bba44c
Garrick Aden-Buie <garrick@adenbuie.com>
2023-05-15 11:12:36 -0400

    Add 313-bslib-card-tab-focus tests

---
+++ inst/apps/313-bslib-card-tab-focus/README.md
@@ -0,0 +1,3 @@
+## 313-bslib-card-tab-focus
+
+`313-bslib-card-tab-focus` tests the tab focus order of full screen cards.
---
+++ inst/helpers/keyboard.R
@@ -0,0 +1,64 @@
+key_press_factory <- function(app) {
+  brwsr <- app$get_chromote_session()
+
+  function(
+    which = "Tab",
+    shift = FALSE,
+    command = FALSE,
+    control = FALSE,
+    alt = FALSE
+  ) {
+    virtual_code <- switch(
+      which,
+      Tab = 9,
+      Enter = 13,
+      Escape = 27,
+      ArrowLeft = 37,
+      ArrowUp = 38,
+      ArrowRight = 39,
+      ArrowDown = 40,
+      Backspace = 8,
+      Delete = 46,
+      Home = 36,
+      End = 35,
+      PageUp = 33,
+      PageDown = 34,
+      Space = 32
+    )
+
+    key <- switch(
+      which,
+      Space = " ",
+      which
+    )
+
+    modifiers <- 0
+    if (shift)   modifiers <- modifiers + 8
+    if (command) modifiers <- modifiers + 4
+    if (control) modifiers <- modifiers + 2
+    if (alt)     modifiers <- modifiers + 1
+
+    events <-
+      brwsr$Input$dispatchKeyEvent(
+        "rawKeyDown",
+        windowsVirtualKeyCode = virtual_code,
+        code = which,
+        key = key,
+        modifiers = modifiers,
+        wait_ = FALSE
+      )$then(
+        brwsr$Input$dispatchKeyEvent(
+          "keyUp",
+          windowsVirtualKeyCode = virtual_code,
+          code = which,
+          key = key,
+          modifiers = modifiers,
+          wait_ = FALSE
+        )
+      )
+
+    brwsr$wait_for(events)
+
+    invisible(app)
+  }
+}
