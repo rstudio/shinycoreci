e7c47cba7987216a5a6e43836597a69e8fec7906
Garrick Aden-Buie <garrick@adenbuie.com>
2023-05-22 14:23:52 -0400

    313: Use a fixed date for dateRangeInput() (#181)

--- inst/apps/313-bslib-card-tab-focus/app.R
+++ inst/apps/313-bslib-card-tab-focus/app.R
@@ -11,7 +11,7 @@ ui <- page_fixed(
       id = "card-no-inputs",
       full_screen = TRUE,
       card_header("Nothing to focus on here"),
-      shiny::p(
+      p(
         "This is a boring card with just some plain text.",
         "There's something to read here but there aren't any inputs to focus on.",
         "Tabbing will only move focus to the \"Close\" button."
@@ -21,7 +21,7 @@ ui <- page_fixed(
       id = "card-with-inputs",
       full_screen = TRUE,
       card_header("Inputs, oh my!"),
-      shiny::p(
+      p(
         "Here's a bit of text! This card does have stuff to focus on, and the",
         "first focusable element is automatically focused when the card is expanded.",
         "Try tabbing through the inputs, you can't leave!"
@@ -33,20 +33,25 @@ ui <- page_fixed(
           id = "card-with-inputs-left",
           full_screen = TRUE,
           card_title("Left Column", class = "mb-3"),
-          shiny::selectInput("letter", "Letter", letters, selected = "a"),
-          shiny::selectizeInput("letter2", "Letter 2", letters, selected = "b", multiple = TRUE),
-          shiny::dateRangeInput("dates", "Pick a Date")
+          selectInput("letter", "Letter", letters, selected = "a"),
+          selectizeInput("letter2", "Letter 2", letters, selected = "b", multiple = TRUE),
+          dateRangeInput(
+            inputId = "dates",
+            label = "Pick a Date",
+            start = "2023-05-01",
+            end = "2023-05-31"
+          )
         ),
         card(
           id = "card-with-inputs-right",
           full_screen = TRUE,
           card_title("Right Column", class = "mb-3"),
-          shiny::sliderInput("slider", "Pick a Number", min = 1, max = 10, value = 5),
-          shiny::textInput("word", "Word", "hello"),
-          shiny::textAreaInput("sentence", "Sentence", "hello world")
+          sliderInput("slider", "Pick a Number", min = 1, max = 10, value = 5),
+          textInput("word", "Word", "hello"),
+          textAreaInput("sentence", "Sentence", "hello world")
         )
       ),
-      shiny::actionButton("go", "Go")
+      actionButton("go", "Go")
     ),
     card(
       id = "card-with-plot",
