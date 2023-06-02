da7b81587a124d634bc1f265b83626c033dc3362
Carson Sievert <cpsievert1@gmail.com>
2023-06-02 17:58:03 -0500

    `bslib::sidebar()` tests (#153)
    Co-authored-by: Garrick Aden-Buie <garrick@adenbuie.com>
    Co-authored-by: cpsievert <cpsievert@users.noreply.github.com>

---
+++ inst/apps/308-sidebar-kitchen-sink/app.R
@@ -0,0 +1,101 @@
+library(shiny)
+library(bslib)
+library(crosstalk)
+library(plotly)
+
+plotly_bars <- plot_ly(x = LETTERS[1:3], y = 1:3) %>%
+  add_bars()
+
+sidebar_long <- sidebar(lorem::ipsum(3, 3))
+sidebar_short <- sidebar(
+  p("A simple sidebar"),
+  actionButton("foo", "This button does nothing")
+)
+
+ui <- page_navbar(
+  title = "Sidebar kitchen sink",
+  fillable = c("Fill", "Fill+Scroll", "Global card sidebar"),
+  id = "navbar",
+  sidebar = sidebar(
+    open = FALSE,
+    position = "right",
+    id = "global_sidebar",
+    bg = "#1E1E1E",
+    shiny::markdown(
+      "Learn more about `bslib::sidebar()` [here](https://rstudio.github.io/bslib/articles/sidebars.html)"
+    )
+  ),
+  header = tagList(
+    tags$style(HTML(".plotly .modebar-container { display: none; }")),
+    span("header", class = "bg-dark"),
+    span("content", class = "bg-dark")
+  ),
+  footer = tagList(
+    span("footer", class = "bg-dark"),
+    span("content", class = "bg-dark")
+  ),
+  nav_panel(
+    "Fill",
+    plotly_bars,
+    layout_sidebar(plotly_bars, sidebar = sidebar_short),
+    card(
+      card_header("Depth"),
+      layout_sidebar(plotly_bars, sidebar = sidebar_short)
+    )
+  ),
+  nav_panel(
+    "Fill+Scroll",
+    plotly_bars,
+    card(
+      card_header("Depth"),
+      layout_sidebar(plotly_bars, sidebar = sidebar_long)
+    ),
+    layout_sidebar(plotly_bars, sidebar = sidebar_long)
+  ),
+  nav_panel(
+    "Scroll",
+    plotly_bars,
+    card(
+      card_header("Depth"),
+      layout_sidebar(plotly_bars, sidebar = sidebar_long)
+    ),
+    layout_sidebar(plotly_bars, sidebar = sidebar_long)
+  ),
+  nav_panel(
+    "Global card sidebar",
+    # Wrapping this up with layout_column_wrap() should keep
+    # the row height the same (even when switch tabs)
+    layout_columns(
+      col_widths = 12,
+      navs_tab_card(
+        title = "Global sidebar",
+        id = "card_tab_sidebar",
+        sidebar = sidebar_long,
+        full_screen = TRUE,
+        nav("Tab 1", plotly_bars, plotly_bars),
+        nav("Tab 2", plotly_bars)
+      ),
+      navs_pill_card(
+        title = "Global sidebar",
+        id = "card_pill_sidebar",
+        sidebar = sidebar_long,
+        full_screen = TRUE,
+        nav("Pill 1", plotly_bars, plotly_bars),
+        nav("Pill 2", plotly_bars)
+      )
+    )
+  ),
+  nav_spacer(),
+  nav_item(actionButton("toggle_sidebar", "Learn more"))
+)
+
+
+server <- function(input, output) {
+
+  observeEvent(input$toggle_sidebar, {
+    sidebar_toggle("global_sidebar")
+  })
+
+}
+
+shinyApp(ui, server)
---
+++ inst/apps/308-sidebar-kitchen-sink/tests/testthat/test-shinytest2.R
@@ -0,0 +1,52 @@
+library(shinytest2)
+
+# Only take screenshots on mac + r-release to reduce diff noise
+release <- rversions::r_release()$version
+release <- paste0(
+  strsplit(release, ".", fixed = TRUE)[[1]][1:2],
+  collapse = "."
+)
+
+is_testing_on_ci <- identical(Sys.getenv("CI"), "true") && testthat::is_testing()
+is_mac_release <- identical(paste0("mac-", release), platform_variant())
+
+DO_SCREENSHOT <- is_testing_on_ci && is_mac_release
+
+test_that("{shinytest2} recording: 308-sidebar-kitchen-sink", {
+  height <- 1200
+  width <- 1600
+
+  app <- AppDriver$new(
+    variant = platform_variant(),
+    name = "308-sidebar-kitchen-sink",
+    view = interactive(),
+    height = height,
+    width = width
+  )
+
+  expect_screenshot <- function() {
+    if (DO_SCREENSHOT) app$expect_screenshot()
+  }
+
+  expect_screenshot()
+
+  app$set_inputs(navbar = "Fill+Scroll")
+  expect_screenshot()
+
+  # Contents should render to their natural height on mobile
+  app$set_window_size(width = 500, height = 1000)
+  expect_screenshot()
+  app$set_window_size(width = width, height = height)
+
+  app$set_inputs(navbar = "Scroll")
+  expect_screenshot()
+
+  app$set_inputs(navbar = "Global card sidebar")
+  app$set_inputs(card_tab_sidebar = "Tab 2")
+  Sys.sleep(1) # Wait for the tab to receive focus
+  expect_screenshot()
+
+  app$click("toggle_sidebar")
+  Sys.sleep(1) # Wait for transition to complete
+  expect_screenshot()
+})
