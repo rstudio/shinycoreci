1e758686a02749d64b205833837b5c631eb1f462
Barret Schloerke <barret@posit.co>
2023-06-06 13:12:50 -0400

    Adjust digits for 194

--- inst/apps/194-reactlog-plot-cache-key/tests/testthat/test-mytest.R
+++ inst/apps/194-reactlog-plot-cache-key/tests/testthat/test-mytest.R
@@ -1,8 +1,12 @@
 library(shinytest2)
 
 test_that("Migrated shinytest test: mytest.R", {
-  app <- AppDriver$new(variant = shinytest2::platform_variant(),
-    seed = 100, shiny_args = list(display.mode = "normal"))
+  app <- AppDriver$new(
+    variant = shinytest2::platform_variant(),
+    seed = 100,
+    shiny_args = list(display.mode = "normal"),
+    options = list("shiny.json.digits" = 4)
+  )
 
   refresh_and_expect <- function() {
     app$set_inputs(`reactlog_module-refresh` = "click")
