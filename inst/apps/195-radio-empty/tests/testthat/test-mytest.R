728fd03353bc3122a09d7163c43ddd458d7243d8
Barret Schloerke <barret@posit.co>
2023-07-05 11:36:42 -0400

    Make 195 more robust to timing issues

--- inst/apps/195-radio-empty/tests/testthat/test-mytest.R
+++ inst/apps/195-radio-empty/tests/testthat/test-mytest.R
@@ -5,10 +5,13 @@ test_that("Migrated shinytest test: mytest.R", {
 
   app$expect_values()
   app$expect_screenshot()
+
   app$set_inputs(radio = "b")
   app$set_inputs(radio2 = "d")
+  app$wait_for_value(input = "radio2")
   app$expect_values()
   app$expect_screenshot()
+
   app$set_inputs(empty = "click")
   app$set_inputs(rerender = "click")
   app$expect_values()
