1fea9d13d0844faf53263353c70c1b11ee04622e
Karan Gathani <karan.gathani@posit.co>
2023-07-11 11:31:06 -0700

    wait longer to ensure test2 has new value

--- inst/apps/195-radio-empty/tests/testthat/test-mytest.R
+++ inst/apps/195-radio-empty/tests/testthat/test-mytest.R
@@ -8,12 +8,13 @@ test_that("Migrated shinytest test: mytest.R", {
 
   app$set_inputs(radio = "b")
   app$set_inputs(radio2 = "d")
-  app$wait_for_value(input = "radio2")
+  txt2_val <- app$wait_for_value(output = "txt2")
   app$expect_values()
   app$expect_screenshot()
 
   app$set_inputs(empty = "click")
   app$set_inputs(rerender = "click")
+  app$wait_for_value(output = "txt2", ignore = list(txt2_val))
   app$expect_values()
   app$expect_screenshot()
 })
