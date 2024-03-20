71a96da693f3c8d556f4627558284f3b7a910465
Carson <cpsievert1@gmail.com>
2024-03-20 14:40:55 -0500

    Set the option in the app itself

--- inst/apps/126-async-ticks/tests/testthat/test-mytest.R
+++ inst/apps/126-async-ticks/tests/testthat/test-mytest.R
@@ -1,13 +1,7 @@
 library(shinytest2)
 
 test_that("Migrated shinytest test: mytest.R", {
-  app <- AppDriver$new(
-    variant = shinytest2::platform_variant(),
-    # Use legacy datatable implementation just for the 
-    # info$datatable$colnames test below. We could, of course, 
-    # update that test, but it's also good to test the legacy.
-    options = list(shiny.legacy.datatable = TRUE)
-  )
+  app <- AppDriver$new(variant = shinytest2::platform_variant())
 
   app$expect_values()
   app$expect_screenshot()
