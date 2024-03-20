f00fa716ec0ae43732e28cc5ccbbb12e5eab13b3
Carson <cpsievert1@gmail.com>
2024-03-20 12:04:13 -0500

    Just use legacy for both

--- inst/apps/126-async-ticks/tests/testthat/test-mytest.R
+++ inst/apps/126-async-ticks/tests/testthat/test-mytest.R
@@ -1,7 +1,13 @@
 library(shinytest2)
 
 test_that("Migrated shinytest test: mytest.R", {
-  app <- AppDriver$new(variant = shinytest2::platform_variant())
+  app <- AppDriver$new(
+    variant = shinytest2::platform_variant(),
+    # Use legacy datatable implementation just for the 
+    # info$datatable$colnames test below. We could, of course, 
+    # update that test, but it's also good to test the legacy.
+    options = list(shiny.legacy.datatable = TRUE)
+  )
 
   app$expect_values()
   app$expect_screenshot()
