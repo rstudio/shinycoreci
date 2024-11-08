b99cc68a5fcd83ce032a044a81fd3647045abbe3
Barret Schloerke <barret@posit.co>
2024-11-08 13:50:01 -0500

    Remove static mac deps to be installed (#275)

--- inst/apps/001-hello/app.R
+++ inst/apps/001-hello/app.R
@@ -1,5 +1,6 @@
 ### Keep this line to manually test this shiny application. Do not edit this line; shinycoreci::::is_manual_app
 
+
 library(shiny)
 
 # Define UI for app that draws a histogram ----
