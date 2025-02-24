c7a73d067e7ebe281d4fb7d32fbdf76b6012f08e
gadenbuie <gadenbuie@users.noreply.github.com>
2025-02-24 20:26:06 +0000

    Generate apps deps (GitHub Actions)

--- R/data-apps-deps.R
+++ R/data-apps-deps.R
@@ -176,6 +176,7 @@ apps_deps_map <- list(
   `316-bslib-popovers` = c("bsicons", "bslib", "plotly", "rversions", "shiny", "shinycoreci", "shinytest2", "testthat", "withr"),
   `317-nav-insert` = c("bslib", "htmltools", "leaflet", "rversions", "shiny", "shinycoreci", "shinytest2", "testthat", "withr"),
   `318-navbar-colors` = c("bslib", "jsonlite", "rlang", "rprojroot", "shiny", "shinytest2", "testthat", "withr"),
+  `319-auto-reload-support` = c("rprojroot", "shiny", "shinytest2", "withr"),
   `900-text-jster` = c("shiny", "shinyjster", "shinytest2"),
   `901-button-jster` = c("shiny", "shinyjster", "shinytest2")
 )
