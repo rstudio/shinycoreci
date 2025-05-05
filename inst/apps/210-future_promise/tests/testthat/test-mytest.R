2a744338e887313ea57b7202fe53d060eecb1846
Barret Schloerke <barret@posit.co>
2025-05-05 16:19:23 -0400

    app(210): Use shinytest2, not shinyjster for testing (#300)
    Co-authored-by: Karan Gathani <karan.gathani@posit.co>

---
+++ inst/apps/210-future_promise/tests/testthat/test-mytest.R
@@ -0,0 +1,41 @@
+skip_if_not_installed("future", "1.21.0")
+
+library(shinytest2)
+
+test_that("Migrated shinytest test: mytest.R", {
+  app <- AppDriver$new(variant = shinytest2::platform_variant())
+
+  # Verify promise counts are higher than future counts
+  expect_counts <- function() {
+    future_counts = as.numeric(app$get_value(output = "future_counts"))
+    promise_counts = as.numeric(app$get_value(output = "promise_counts"))
+
+    str(list(
+      future_counts = future_counts,
+      promise_counts = promise_counts
+    ))
+
+    expect_gt(promise_counts, future_counts)
+  }
+
+  wait_for_idle <- function() {
+    app$wait_for_idle(duration = 3 * 1000, timeout = 30 * 1000)
+  }
+
+  app$click("go_future_future")
+  wait_for_idle()
+
+  app$click("go_future_promise")
+  wait_for_idle()
+
+  expect_counts()
+
+  # again
+  app$click("go_future_future")
+  wait_for_idle()
+
+  app$click("go_future_promise")
+  wait_for_idle()
+
+  expect_counts()
+})
