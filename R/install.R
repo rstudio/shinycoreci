bcd5d0469cf3d9c6113b066668be4bce8ee88833
Barret Schloerke <barret@posit.co>
2025-05-16 14:13:41 -0400

    Update R/install.R

--- R/install.R
+++ R/install.R
@@ -237,7 +237,6 @@ install_pkgs_with_callr <- function(
       # Avoids weird installs when using pak to install shinycoreci
       stopifnot(utils::packageVersion("pak") >= "0.3.0")
       pak__pkg_install <- utils::getFromNamespace("pkg_install", "pak")
-      print(packageDescription("pak"))
       message(
         "Installing packages with pak::pkg_install(): ",
         paste0(packages, collapse = ", ")
