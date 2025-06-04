5a3b4a33fa519fa11dd6638572473acf2f71018e
Karan Gathani <karan.gathani@posit.co>
2025-06-04 08:42:45 -0700

    add fns to skip tests if not mac or mac OS

--- inst/apps/118-highcharter-births/tests/testthat/setup.R
+++ inst/apps/118-highcharter-births/tests/testthat/setup.R
@@ -1,2 +1,14 @@
 # Load application support files into testing environment
 shinytest2::load_app_env()
+
+skip_if_not_macos <- function() {
+    if (Sys.info()[["sysname"]] != "Darwin") {
+        testthat::skip("Test only runs on macOS")
+    }
+}
+
+skip_if_not_linux_or_windows <- function() {
+    if (Sys.info()[["sysname"]] == "Darwin") {
+        testthat::skip("Test only runs on Linux or Windows")
+    }
+}
