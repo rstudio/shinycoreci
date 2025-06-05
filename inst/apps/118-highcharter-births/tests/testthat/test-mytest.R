6b1edc52557c7785f2084e410b33471b6f8385e1
Karan <karangattu@users.noreply.github.com>
2025-06-05 16:24:20 -0700

    test(118-highcharter-births): Use assertion based tests (#310)
    * add fns to skip tests if not mac or mac OS
    
    * use assertion based tests on for windows & linux
    
    * formatting the tests
    
    * Generate apps deps (GitHub Actions)
    
    * remove unused snaps
    
    * Generate apps deps (GitHub Actions)

--- inst/apps/118-highcharter-births/tests/testthat/test-mytest.R
+++ inst/apps/118-highcharter-births/tests/testthat/test-mytest.R
@@ -1,5 +1,23 @@
 library(shinytest2)
 
+expect_screenshot_mac_release <- local({
+  release <- jsonlite::fromJSON("https://api.r-hub.io/rversions/resolve/release")$version
+  release <- paste0(
+    strsplit(release, ".", fixed = TRUE)[[1]][1:2],
+    collapse = "."
+  )
+
+  is_testing_on_ci <- identical(Sys.getenv("CI"), "true") && testthat::is_testing()
+  is_mac_release <- identical(paste0("mac-", release), platform_variant())
+
+  DO_SCREENSHOT <- is_testing_on_ci && is_mac_release
+  function(app, ..., threshold = 2) {
+    if (!DO_SCREENSHOT) return(invisible(app))
+
+    app$expect_screenshot(..., threshold = threshold)
+  }
+})
+
 test_that("Migrated shinytest test: mytest.R", {
   app <- AppDriver$new(
     variant = shinytest2::platform_variant(),
@@ -10,7 +28,7 @@ test_that("Migrated shinytest test: mytest.R", {
 
   Sys.sleep(2) # wait for widget
   app$expect_values()
-  app$expect_screenshot()
+  expect_screenshot_mac_release(app)
 
   app$set_inputs(year = c(1994, 2005))
   app$set_inputs(year = c(1997, 2005))
@@ -18,5 +36,5 @@ test_that("Migrated shinytest test: mytest.R", {
   app$set_inputs(theme = "fivethirtyeight")
   Sys.sleep(2) # wait for widget / css
   app$expect_values()
-  app$expect_screenshot()
+  expect_screenshot_mac_release(app)
 })
