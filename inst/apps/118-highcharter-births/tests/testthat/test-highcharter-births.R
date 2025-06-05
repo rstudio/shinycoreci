1a0bbf0485a1d12c778e9b8441809ec709d70762
Karan <karangattu@users.noreply.github.com>
2025-06-05 16:24:20 -0700

    test(118-highcharter-births): Use assertion based tests (#310)
    * add fns to skip tests if not mac or mac OS
    
    * use assertion based tests on for windows & linux
    
    * formatting the tests
    
    * Generate apps deps (GitHub Actions)
    
    * remove unused snaps
    
    * Generate apps deps (GitHub Actions)

---
+++ inst/apps/118-highcharter-births/tests/testthat/test-highcharter-births.R
@@ -0,0 +1,42 @@
+library(shinytest2)
+library(testthat)
+
+test_that("Days of week and scatter points are present in chart", {
+    app <- AppDriver$new(
+        variant = shinytest2::platform_variant(),
+        seed = 100
+    )
+    app$wait_for_value(input = "plot_type")
+    app$set_inputs(plot_type = "scatter")
+    app$wait_for_idle()
+
+    # asserting all 7 days of the week show up within charts
+    expected_days <- c(
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday",
+        "Sunday"
+    )
+
+    days_text <- app$get_text(".highcharts-xaxis-labels text")
+    expect_true(all(expected_days %in% days_text))
+
+    # Asserting only 7 scatter points in the charts
+    points <- app$get_html(".highcharts-point")
+    expect_length(points, 7)
+
+    # asserting the chart has a width and height greater than 10
+    width <- app$get_js(
+        "document.querySelector('.highcharts-background').getAttribute('width')"
+    )
+    height <- app$get_js(
+        "document.querySelector('.highcharts-background').getAttribute('height')"
+    )
+    expect_gt(as.numeric(width), 10)
+    expect_gt(as.numeric(height), 10)
+
+    app$stop()
+})
