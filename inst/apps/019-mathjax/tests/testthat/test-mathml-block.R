56ec145648e6567bb00690c629025a53857740e2
Karan Gathani <karan.gathani@posit.co>
2025-06-05 12:36:43 -0700

    remove unused snaps

--- inst/apps/019-mathjax/tests/testthat/test-mathml-block.R
+++ inst/apps/019-mathjax/tests/testthat/test-mathml-block.R
@@ -4,7 +4,7 @@ library(testthat)
 test_that("MathML block contains α2", {
   app <- AppDriver$new(variant = shinytest2::platform_variant())
 
-  app$wait_for_js("$('#ex1 .MJX_Assistive_MathML.MJX_Assistive_MathML_Block').length > 0", timeout = 3000)
+  app$wait_for_js("$('#ex1 .MJX_Assistive_MathML.MJX_Assistive_MathML_Block').length > 0", timeout = 10 * 1000)
   math_text <- app$get_text("#ex1 .MJX_Assistive_MathML.MJX_Assistive_MathML_Block")
 
   expect_equal(math_text, "α2")
