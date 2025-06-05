32296a060d41efde63c17fef1aa9b2616c047831
Karan Gathani <karan.gathani@posit.co>
2025-06-05 12:51:35 -0700

    use correct implementation

--- inst/apps/019-mathjax/tests/testthat/test-mytest.R
+++ inst/apps/019-mathjax/tests/testthat/test-mytest.R
@@ -13,23 +13,24 @@ expect_screenshot_mac_release <- local({
   DO_SCREENSHOT <- is_testing_on_ci && is_mac_release
   function(app, ..., threshold = 2) {
     if (!DO_SCREENSHOT) return(invisible(app))
-
     app$expect_screenshot(..., threshold = threshold)
   }
 })
 
-
 test_that("Migrated shinytest test: mytest.R", {
-  app <- AppDriver$new(variant = shinytest2::platform_variant(),
-    load_timeout = 15000, seed = 100, shiny_args = list(display.mode = "normal"))
+  app <- AppDriver$new(
+    variant = shinytest2::platform_variant(),
+    load_timeout = 15000,
+    seed = 100,
+    shiny_args = list(display.mode = "normal")
+  )
 
   Sys.sleep(3) # wait for mathjax to process
   app$expect_values()
-  app$expect_screenshot_mac_release(app
-
+  expect_screenshot_mac_release(app)
 
   app$set_inputs(ex5_visible = TRUE)
   Sys.sleep(2) # wait for mathjax to process
   app$expect_values()
-  app$expect_screenshot_mac_release(app)
+  expect_screenshot_mac_release(app)
 })
