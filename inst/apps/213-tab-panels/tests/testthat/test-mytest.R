676bab6da1925e12c8e96c2832d36ccdcedf3d20
Barret Schloerke <barret@posit.co>
2025-12-11 13:15:45 -0500

    Lenient 213 screenshot snapshots

--- inst/apps/213-tab-panels/tests/testthat/test-mytest.R
+++ inst/apps/213-tab-panels/tests/testthat/test-mytest.R
@@ -1,47 +1,50 @@
 library(shinytest2)
 
 test_that("Migrated shinytest test: mytest.R", {
-  app <- AppDriver$new(variant = shinytest2::platform_variant(),
-    seed = 100, shiny_args = list(display.mode = "normal"))
+  app <- AppDriver$new(
+    variant = shinytest2::platform_variant(),
+    seed = 100,
+    shiny_args = list(display.mode = "normal")
+  )
 
   app$expect_values()
-  app$expect_screenshot()
+  app$expect_screenshot(threshold = 2)
 
   # Add Dynamic-1
   app$set_inputs(add = "click")
   app$set_inputs(tabs = "Dynamic-1")
   app$expect_values()
-  app$expect_screenshot()
+  app$expect_screenshot(threshold = 2)
 
   # Remove Foo tabs
   app$set_inputs(removeFoo = "click")
   app$expect_values()
-  app$expect_screenshot()
+  app$expect_screenshot(threshold = 2)
 
   # Add Foo-1 (also opens tab)
   app$set_inputs(addFoo = "click")
   app$expect_values()
-  app$expect_screenshot()
+  app$expect_screenshot(threshold = 2)
 
   # View Default Hello tab
   app$set_inputs(tabs = "Hello")
   app$expect_values()
-  app$expect_screenshot()
+  app$expect_screenshot(threshold = 2)
 
   # Add Dynamic-2 tab and show it in menu. Do not show Dynamic-2 tab.
   app$set_inputs(add = "click")
   app$click(selector = ".dropdown-toggle")
   app$expect_values()
-  app$expect_screenshot()
+  app$expect_screenshot(threshold = 2)
   app$click(selector = ".dropdown-toggle")
 
   # Add Foo-2 tab (also opens tab)
   app$set_inputs(addFoo = "click")
   app$expect_values()
-  app$expect_screenshot()
+  app$expect_screenshot(threshold = 2)
 
   # Remove second Foo tab
   app$set_inputs(removeFoo = "click")
   app$expect_values()
-  app$expect_screenshot()
+  app$expect_screenshot(threshold = 2)
 })
